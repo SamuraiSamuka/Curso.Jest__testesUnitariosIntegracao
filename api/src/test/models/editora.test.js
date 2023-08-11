eb610dec073f33e8714e3a33c2eb3ebfdd1d4d18
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-08-11 14:01:21 -0300

    organizando o projeto para testar apis

--- api/src/test/models/editora.test.js
+++ api/src/test/models/editora.test.js
@@ -1,4 +1,16 @@
-import editora from '../../models/editora.js'
-import { describe } from '@jest/globals'
+import { describe, expect, it } from '@jest/globals';
+import Editora from '../../models/editora.js';
 
-des
+describe('Testando o modelo editora', () => {
+  const objetoEditora = {
+    nome: 'CDC',
+    cidade: 'Sao Paulo',
+    email: 'c@c.com',
+  };
+
+  it('Deve instanciar uma nova editora', () => {
+    const editora = new Editora(objetoEditora);
+
+    expect(editora).toEqual(expect.objectContaining(objetoEditora));
+  });
+});
